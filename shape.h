cc0b228a79bdad10d5e6fc3d15268e5babd04171
Hemanth S A <hemanth.s.a@gmail.com>
2014-04-26 12:24:52 -0400

    Added detail

--- shape.h
+++ shape.h
@@ -32,10 +32,21 @@ public:
 	    return bbox;
 	}
 	
+	// draws this shape on to the screen
 	void draw ();
+	
+	// if the mouse button was pressed inside this shape, the shape is grabbed for movement.
 	void grab_object (const double xpos, const double ypos);
+	
+	// this function calculates the minimum and maximum values of x, y and z coordinates
+	// of the shape
 	void get_max_min_vertex (vertex *max, vertex *min);
+	
+	// releases the shape when the mouse button is released
 	void release_object ();
+	
+	// this function is responsible for moving the shape around the screen
+	// dragging the object
 	void move_shape (const double xpos, const double ypos);
 };
 
